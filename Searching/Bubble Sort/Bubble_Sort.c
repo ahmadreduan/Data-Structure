4dc431934aca5844e8105b5b3258bc9fef75113f
ahmadreduan <reduanahmad363737@gmail.com>
2024-09-19 03:16:06 +0600

    bubbleSort

---
+++ Searching/Bubble Sort/Bubble_Sort.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void babble_Sort(int arr[],int size){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]= temp;
+            }
+        }
+    }
+
+    printf("Sorted Array : ");
+    for(int i=0 ;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+int main(){
+    int size ;
+    
+    printf("Enter the size of array : ");
+    scanf("%d",&size);
+    
+    int arr[size];
+    printf("Enter the eliment of the array : ");
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    printf("The eliment is : ");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+
+    babble_Sort(arr,size);
+
+    return 0;
+}
