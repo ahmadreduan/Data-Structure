5bf1995a5051a4e2bb94f254fdfb3c8aa1056f89
ahmadreduan <reduanahmad363737@gmail.com>
2024-09-22 23:16:34 +0600

    Deleting an Element from a 1D Array

---
+++ Array/Delete_element.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int arr[10] = {1, 2, 3, 4, 5};
+    int size = 5;
+    int pos;
+
+    printf("Enter the index of the element to delete : ");
+    scanf("%d", &pos);
+
+    if (pos < 0 || pos >= size)
+    {
+        perror("Invalid index !");
+        return -1;
+    }
+
+    for (int i = pos; i < size - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    size--;
+
+    printf("Array after deletion: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
