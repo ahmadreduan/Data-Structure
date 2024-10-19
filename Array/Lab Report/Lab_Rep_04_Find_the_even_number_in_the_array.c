6503ec48383fdc07493d40584d936ae7f9c25a7f
Reduan Ahmad <reduanahmad363737@gmail.com>
2024-10-19 18:20:55 +0600

    Find the Maximun element in the array.

---
+++ Array/Lab Report/Lab_Rep_04_Find_the_even_number_in_the_array.c
@@ -0,0 +1,35 @@
+/*Name : Reduan Ahmad
+  ID : 232-35-016
+  Section : A2
+  Problem : Find the Maximun element in the array.
+*/
+#include <stdio.h>
+int main()
+{
+    int size, index, element;
+    printf("Enter the array size : ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the element :");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("The array is :");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+        printf("The Even number in the array :");
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i]%2 == 0)
+        {
+            printf("%d ", arr[i]);
+        }
+    }
+}
